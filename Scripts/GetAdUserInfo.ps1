65919578d7caf48680730635503cd9b01835a0b9
AngeloPO83 <ar6238@gmail.com>
2025-02-26 23:48:13 -0800

    Update GetAdUserInfo.ps1

--- Scripts/GetAdUserInfo.ps1
+++ Scripts/GetAdUserInfo.ps1
@@ -1,3 +1,21 @@
+<#
+List Users in Active Directory – PowerShell GUI Tool (2019)
+Developed by Angelo Polatto.
+
+Description:
+  This script provides a GUI-based application to retrieve and display user information from Active Directory.
+  It allows administrators to search for users by username, email, or display name and view detailed attributes including group memberships and account status.
+  It also supports exporting the search results to CSV.
+
+Requirements:
+  - Active Directory module (RSAT-AD-PowerShell) must be installed.
+  - The executing user must have the necessary permissions to query Active Directory.
+
+Note:
+  This project was developed as a study case to explore PowerShell GUI development, AD automation, and robust scripting practices.
+  Feedback and suggestions for improvements are welcome.
+#>
+
 Add-Type -AssemblyName System.Windows.Forms
 [System.Windows.Forms.Application]::EnableVisualStyles()
 $ProgressPreference = 'SilentlyContinue'
